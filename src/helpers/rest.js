fa9978e9a822a59e08bd2a70c2d7521bbc447771
arsenicum32 <zombiehot@yandex.ru>
2017-11-05 14:45:34 +0300

    test

--- src/helpers/rest.js
+++ src/helpers/rest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ins = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://46.101.126.64:5000/',
   headers: {'X-Custom-Header': 'foobar'}
 });
 
