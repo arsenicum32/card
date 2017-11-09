66535d3758a83dc37d7db18eea412e7d0ddd7c55
arsenicum32 <zombiehot@yandex.ru>
2017-11-09 15:20:25 +0300

    test

---
+++ main.php
@@ -0,0 +1,53 @@
+<?php
+include 'conf.php';
+
+if(isset($_GET['from'])){
+    $from = $_GET['from'];
+    if(isset($_GET['to'])){
+        $to = $_GET['to'];
+    }else{
+        $to = time();
+    }
+    $q = " WHERE time BETWEEN FROM_UNIXTIME(".$from.") AND FROM_UNIXTIME(".$to.")";
+}else{
+    $q = "";
+}
+
+
+$sql = "SELECT * FROM log".$q;
+$result = $conn->query($sql);
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    $stack = array();
+    $res = [
+      [
+        'загрузок карты',
+        0,0,0,0
+      ],
+      [
+        'ср. время работы',
+        0,0,0,0
+      ],
+      [
+
+        'сформированных МРД',
+        0,0,0,0
+      ],
+      [
+        'загрузок в Excel',
+        0,0,0,0
+      ]
+    ];
+    while($row = $result->fetch_assoc()) {
+        array_push($stack, [uid=> $row["uid"], time=> strtotime($row["time"]), stime=> $row["sitetime"] , action=> $row["action"],more=> $row["more"]]);
+    }
+    echo json_encode($res);
+} else {
+    echo json_encode($res);
+}
+
+$conn->close();
